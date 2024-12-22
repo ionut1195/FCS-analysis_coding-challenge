7ce2941d0022cb4cc831aa05147a8ad4b0f5c6c5
ionut1195 <ionut_tomescu95@yahoo.com>
2024-12-22 09:00:55 +0200

    defined main function

--- src/main.py
+++ src/main.py
@@ -1,21 +1,28 @@
 from scatter_plot_generator import ScatterPlotGenerator
 from data_loader import FCSLoader
 from pathlib import Path
-from itertools import combinations
 
-loader = FCSLoader()
-meta, data = loader.load_file("./src/0001[WDF].fcs")
-channels = loader.get_channels(meta)
 
-# Create dictionary of all channel data
-channel_data = {channel: data[channel] for channel in channels}
+def main():
+    loader = FCSLoader()
+    meta, data = loader.load_file("./src/0001[WDF].fcs")
+    channels = loader.get_channels(meta)
 
-# Initialize generator
-generator = ScatterPlotGenerator()
+    # Create dictionary of all channel data
+    channel_data = {channel: data[channel] for channel in channels}
 
-output_dir = Path("scatter_plots")
-output_dir.mkdir(exist_ok=True)
+    # Initialize generator
+    generator = ScatterPlotGenerator()
 
-generated_plots = generator.generate_scatter_matrix(channel_data, save_dir=output_dir)
-for i in generated_plots:
-    generator.verify_output(i)
+    output_dir = Path("scatter_plots")
+    output_dir.mkdir(exist_ok=True)
+
+    generated_plots = generator.generate_scatter_matrix(
+        channel_data, save_dir=output_dir
+    )
+    for i in generated_plots:
+        generator.verify_output(i)
+
+
+if __name__ == "__main__":
+    main()
